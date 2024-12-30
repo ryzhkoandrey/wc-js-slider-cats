c272750eb95376e024618f150ceab32be22b5ed3
Андрей Рыжко <ryzhkoandrey@gmail.com>
2024-12-30 19:32:34 +0200

    +

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Элементы на странице
+const slider = document.querySelector('#slider');
+const sliderItems = Array.from(slider.children);
+
+sliderItems.forEach(function (slide, index) {
+
+    // Скрываем все слайды, кроме первого
+    if (index !== 0) {
+        slide.classList.add('hidden');
+    }
+
+    // Добавляем индексы
+    slide.dataset.index = index;
+
+    // Клик по слайдам
+    slide.addEventListener('click', function () {
+        // Скрываем текущий слайд
+        this.classList.add('hidden');
+
+        // Рассчитываем индекс следующего слайда
+
+
+        // const nextSlideIndex = +slide.dataset.index + 1;
+
+        // Находим следующий слайд
+        const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+
+        // Отображаем следующий слайд
+        nextSlide.classList.remove('hidden');
+    });
+});
